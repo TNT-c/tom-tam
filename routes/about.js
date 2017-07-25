846f9447300785b29f9a8fcd6a39de75dfaae15c
tomtam6 <tomtam6@gmail.com>
2017-07-25 11:35:47 -0400

    Added contact page

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET about page. */
 router.get('/', function(req, res, next) {
-  res.render('about');
+  res.render('about', { title: 'Tom Tam - About' });
 });
 
 module.exports = router;
