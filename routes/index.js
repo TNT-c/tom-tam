bd2bad327f6b96406ffe27da639be38360aeffcb
tomtam6 <tomtam6@gmail.com>
2017-07-25 09:24:35 -0400

    Changed name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Tom Tam' });
 });
 
 module.exports = router;
