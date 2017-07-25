4bf6ca39163e5667e4eeab0126043a0d465ab876
tomtam6 <tomtam6@gmail.com>
2017-07-25 10:21:07 -0400

    Added banner

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET about page. */
 router.get('/', function(req, res, next) {
-  res.render('about', { title: 'Thomas' });
+  res.render('about', { title: 'Tom Tam - About' });
 });
 
 module.exports = router;
